3e78ee8b0cd803e52c5aa1fdf3d9be0ac038c7be
Anderson David SF <andersondavidprofissional@gmail.com>
2024-04-01 13:15:02 -0300

    feat: create headline

---
+++ src/app/components/main/headline.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Headline() {
+  return (
+    <div className="relative top-1/4">
+      <h1 className="text-gray-500 text-[12em] font-impact">GAMEPAD TESTER</h1>
+    </div>
+  );
+}
