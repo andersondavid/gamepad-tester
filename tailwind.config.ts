9aa630a3fbde9885000d891a1b1a8d679c4d1b42
Anderson David SF <andersondavidprofissional@gmail.com>
2024-04-01 13:15:16 -0300

    feat: add font family impact

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,9 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-    },
+    fontFamily: {
+      'impact': ['Impact']
+    }
   },
   plugins: [],
 };
