84a365ca5621ac8636b38ff52d93ee7878bff729
Anderson David SF <andersondavidprofissional@gmail.com>
2024-04-01 13:15:46 -0300

    feat: create header and secondline

---
+++ src/app/components/main/secondLine.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function PressAny() {
+  return (
+    <div className="">
+      <p className="text-gray-300 text-4xl w-48">
+        Press any <span className="text-orange-400">button</span>.
+      </p>
+    </div>
+  );
+}
