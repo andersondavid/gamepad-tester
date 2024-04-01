f550084a6d454fd37af12ccb28b13a6dc5a65a04
Anderson David SF <andersondavidprofissional@gmail.com>
2024-04-01 13:14:04 -0300

    fix: remove rules for position from gamepad file

--- src/app/components/gamepad/index.tsx
+++ src/app/components/gamepad/index.tsx
@@ -3,11 +3,9 @@ import React from "react";
 
 export default function Gamepad() {
   return (
-    <div className="relative w-[1000px] h-[1000px] top-2/4 top-[10%] transition-all duration-1000">
-      <div className="">
+    <div className="w-[1000px] h-[1000px] relative">
         <Image className="absolute" src={"/gamepad-base.png"} alt="Gamepad for xbox" objectFit="cover" fill={true}/>
         <Image className="absolute" src={"/pressed.png"} alt="Gamepad for xbox" objectFit="cover" fill={true}/>
-      </div>
     </div>
   );
 }
