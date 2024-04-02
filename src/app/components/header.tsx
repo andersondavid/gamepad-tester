b24da27ab563de10f73f321401b5feac94b918c1
Anderson David SF <andersondavidprofissional@gmail.com>
2024-04-02 18:51:08 -0300

    fix: set the page to the initial mode

---
+++ src/app/components/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="flex justify-between">
+      <div className="p-8">
+        <h2 className="text-white text-3xl font-bold">ADFS</h2>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,25 +13,19 @@ export default function Home() {
       <div className="fixed top-0 left-0 w-full h-full flex justify-center">
         <Headline />
       </div>
-      <div className="fixed bottom-0 left-1/2 -translate-x-1/2">
+      <div className="fixed bottom-[-30%] left-1/2 -translate-x-1/2">
         <Gamepad />
-        <div className="fixed top-4 left-1/2 -translate-x-1/2">
+        <div className="fixed top-4 left-1/2 -translate-x-1/2 opacity-0">
           <SholderButtonsContainer />
         </div>
         <div className="absolute w-1/2 top-[65%] left-1/2 -translate-x-1/2">
           <TesterConfigs />
         </div>
       </div>
-      {/* <div className="fixed bottom-1/4 w-full">
-        <div className="flex justify-between max-w-screen-lg m-auto">
-          <PressAny />
-          <About />
-        </div>
-      </div> */}
       <div className="fixed bottom-0 left-0">
         <AboutMe />
       </div>
-      <div className="fixed right-0 top-28">
+      <div className="fixed right-0 top-28 hidden">
         <BottonsFeed />
       </div>
     </main>
