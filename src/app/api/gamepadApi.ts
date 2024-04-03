696e9ae2772b63473866f602c3d8749a69b2e2a0
Anderson David SF <andersondavidprofissional@gmail.com>
2024-04-02 21:38:04 -0300

    feat: gamepad api ready

--- src/app/api/gamepadApi.ts
+++ src/app/api/gamepadApi.ts
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
 
 function useGamepadConnected() {
-  const [isGamepadConnected, setGamepadConnected] = useState<GamepadEvent | boolean>(false);
+  const [isGamepadConnected, setGamepadConnected] = useState<
+    GamepadEvent | boolean
+  >(false);
   useEffect(() => {
     function handleGamepadConnected(e: GamepadEvent) {
       setGamepadConnected(e);
-      console.log('connected');
-      
+      console.log("connected");
     }
     function handleGamepadDesconnected() {
       setGamepadConnected(false);
-      console.log('desconnected');
-
+      console.log("desconnected");
     }
     window.addEventListener("gamepadconnected", handleGamepadConnected);
     window.addEventListener("gamepaddisconnected", handleGamepadDesconnected);
@@ -22,9 +22,22 @@ function useGamepadConnected() {
     };
   }, []);
 
-  return isGamepadConnected
+  return isGamepadConnected;
 }
 
-export {
-  useGamepadConnected
+function useGetButtons(isGamepadConnected: GamepadEvent | boolean) {
+  const [gamepadState , setGamepadState] = useState<(Gamepad | null)>()
+  useEffect(() => {
+
+    const intervalId = setInterval(() => {
+      setGamepadState(navigator.getGamepads()[0]);
+    }, 60);
+    
+    return () => clearInterval(intervalId);
+  }, [isGamepadConnected]);
+
+  return gamepadState
+
 }
+
+export { useGamepadConnected, useGetButtons };
