b6a703b990fcb53e362028f325640f8987637962
Anderson David SF <andersondavidprofissional@gmail.com>
2024-04-02 21:38:30 -0300

    feat: feed buttons now with api

--- src/app/components/main/components/BottonsFeed.tsx
+++ src/app/components/main/components/BottonsFeed.tsx
@@ -1,23 +1,14 @@
-import React from 'react'
-import ButtonItem from './ButtonItem'
+import React from "react";
+import ButtonItem from "./ButtonItem";
+import { useGetButtons, useGamepadConnected } from "@/app/api/gamepadApi";
 
 export default function BottonsFeed() {
+  const getbuttons = useGetButtons(useGamepadConnected());
   return (
-    <div className='w-36'>
-      <ButtonItem />
-      <ButtonItem />
-      <ButtonItem />
-      <ButtonItem />
-      <ButtonItem />
-      <ButtonItem />
-      <ButtonItem />
-      <ButtonItem />
-      <ButtonItem />
-      <ButtonItem />
-      <ButtonItem />
-      <ButtonItem />
-      <ButtonItem />
-      <ButtonItem />
+    <div className="w-36">
+      {getbuttons?.buttons.map((button, i) => (
+        <ButtonItem key={"B" + i} index={i} value={button.value} />
+      ))}
     </div>
-  )
+  );
 }
