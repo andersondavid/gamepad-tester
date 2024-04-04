14587d1b2a2f8aa66a87437bdc77b6094db22601
Anderson David SF <andersondavidprofissional@gmail.com>
2024-04-03 23:42:53 -0300

    fix: add preloader to highlight to improve perfomace

--- src/app/components/gamepad/index.tsx
+++ src/app/components/gamepad/index.tsx
@@ -1,26 +1,116 @@
 import { useGamepadConnected, useGetButtons } from "@/app/api/gamepadApi";
 import Image from "next/image";
 import React from "react";
+import AxesGraphic from "./AxesGraphic";
 
 export default function Gamepad() {
-
   const getbuttons = useGetButtons(useGamepadConnected());
-  const bt = getbuttons?.buttons || []
+  const bt = getbuttons?.buttons || [];
 
   return (
-    <div className="w-[1000px] h-[1000px] relative">
-        <Image className="absolute" src={"/gamepad-base.png"} alt="Gamepad for xbox" objectFit="cover" fill={true}/>
-        {bt[0]?.value > 0 && <Image className="absolute" src={"/buttons_highlight/a.png"} alt="Gamepad for xbox" objectFit="cover" fill={true}/>}
-        {bt[1]?.value > 0  && <Image className="absolute" src={"/buttons_highlight/b.png"} alt="Gamepad for xbox" objectFit="cover" fill={true}/>}
-        {bt[3]?.value > 0  && <Image className="absolute" src={"/buttons_highlight/y.png"} alt="Gamepad for xbox" objectFit="cover" fill={true}/>}
-        {bt[2]?.value > 0  && <Image className="absolute" src={"/buttons_highlight/x.png"} alt="Gamepad for xbox" objectFit="cover" fill={true}/>}
-        {bt[13]?.value > 0  && <Image className="absolute" src={"/buttons_highlight/down.png"} alt="Gamepad for xbox" objectFit="cover" fill={true}/>}
-        {bt[14]?.value > 0  && <Image className="absolute" src={"/buttons_highlight/left.png"} alt="Gamepad for xbox" objectFit="cover" fill={true}/>}
-        {bt[12]?.value > 0  && <Image className="absolute" src={"/buttons_highlight/up.png"} alt="Gamepad for xbox" objectFit="cover" fill={true}/>}
-        {bt[15]?.value > 0  && <Image className="absolute" src={"/buttons_highlight/right.png"} alt="Gamepad for xbox" objectFit="cover" fill={true}/>}
-        {bt[9]?.value > 0  && <Image className="absolute" src={"/buttons_highlight/start.png"} alt="Gamepad for xbox" objectFit="cover" fill={true}/>}
-        {bt[8]?.value > 0  && <Image className="absolute" src={"/buttons_highlight/select.png"} alt="Gamepad for xbox" objectFit="cover" fill={true}/>}
-        {bt[16]?.value > 0  && <Image className="absolute" src={"/buttons_highlight/home.png"} alt="Gamepad for xbox" objectFit="cover" fill={true}/>}
+    <div className="w-[1000px] h-[1000px] relative border-gray-50">
+      <Image
+        className="absolute"
+        src={"/gamepad-base.png"}
+        alt="Gamepad for xbox"
+        objectFit="cover"
+        fill={true}
+      />
+
+      <Image
+        className={`absolute ${bt[0]?.value ? "opacity-100" : "opacity-0"}`}
+        src={"/buttons_highlight/a.png"}
+        alt="Gamepad for xbox"
+        objectFit="cover"
+        fill={true}
+      />
+
+      <Image
+        className={`absolute ${bt[1]?.value ? "opacity-100" : "opacity-0"}`}
+        src={"/buttons_highlight/b.png"}
+        alt="Gamepad for xbox"
+        objectFit="cover"
+        fill={true}
+      />
+
+      <Image
+        className={`absolute ${bt[3]?.value ? "opacity-100" : "opacity-0"}`}
+        src={"/buttons_highlight/y.png"}
+        alt="Gamepad for xbox"
+        objectFit="cover"
+        fill={true}
+      />
+
+      <Image
+        className={`absolute ${bt[2]?.value ? "opacity-100" : "opacity-0"}`}
+        src={"/buttons_highlight/x.png"}
+        alt="Gamepad for xbox"
+        objectFit="cover"
+        fill={true}
+      />
+
+      <Image
+        className={`absolute ${bt[13]?.value ? "opacity-100" : "opacity-0"}`}
+        src={"/buttons_highlight/down.png"}
+        alt="Gamepad for xbox"
+        objectFit="cover"
+        fill={true}
+      />
+
+      <Image
+        className={`absolute ${bt[14]?.value ? "opacity-100" : "opacity-0"}`}
+        src={"/buttons_highlight/left.png"}
+        alt="Gamepad for xbox"
+        objectFit="cover"
+        fill={true}
+      />
+
+      <Image
+        className={`absolute ${bt[12]?.value ? "opacity-100" : "opacity-0"}`}
+        src={"/buttons_highlight/up.png"}
+        alt="Gamepad for xbox"
+        objectFit="cover"
+        fill={true}
+      />
+
+      <Image
+        className={`absolute ${bt[15]?.value ? "opacity-100" : "opacity-0"}`}
+        src={"/buttons_highlight/right.png"}
+        alt="Gamepad for xbox"
+        objectFit="cover"
+        fill={true}
+      />
+
+      <Image
+        className={`absolute ${bt[9]?.value ? "opacity-100" : "opacity-0"}`}
+        src={"/buttons_highlight/start.png"}
+        alt="Gamepad for xbox"
+        objectFit="cover"
+        fill={true}
+      />
+
+      <Image
+        className={`absolute ${bt[8]?.value ? "opacity-100" : "opacity-0"}`}
+        src={"/buttons_highlight/select.png"}
+        alt="Gamepad for xbox"
+        objectFit="cover"
+        fill={true}
+      />
+
+      <Image
+        className={`absolute ${bt[16]?.value ? "opacity-100" : "opacity-0"}`}
+        src={"/buttons_highlight/home.png"}
+        alt="Gamepad for xbox"
+        objectFit="cover"
+        fill={true}
+      />
+
+      <div className="absolute top-[358px] left-[267px]">
+        <AxesGraphic />
+      </div>
+      <div className="absolute top-[496px] left-[621px]">
+        <AxesGraphic />
+      </div>
     </div>
   );
 }
