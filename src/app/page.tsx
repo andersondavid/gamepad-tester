7d75d4d4f690930758401ee44ebc6174294d3cf4
Anderson David SF <andersondavidprofissional@gmail.com>
2024-04-03 23:52:16 -0300

    refactor: remove configure optins from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import Gamepad from "./components/gamepad";
 import Header from "./components/header";
 import Headline from "./components/main/Headline";
 import AboutMe from "./components/main/AboutMe";
-import TesterConfigs from "./components/main/TesterConfigs";
 import BottonsFeed from "./components/main/components/BottonsFeed";
 import SholderButtonsContainer from "./components/gamepad/SholderButtonsContainer";
 
@@ -14,9 +13,7 @@ import { useGamepadConnected } from "./api/gamepadApi";
 
 export default function Home() {
   const gamepad = useGamepadConnected();
-
   const [gamepadOnline, setGamepadOnline] = useState<boolean>(false);
-  const [showButtonsFeed, setShowButtonsFeed] = useState<boolean>(false);
 
   useEffect(() => {
     if (gamepad != false) {
@@ -25,8 +22,6 @@ export default function Home() {
       setGamepadOnline(false);
     }
   }, [gamepad]);
-
-  useEffect(() => {}, [showButtonsFeed]);
 
   return (
     <main className="relative">
@@ -47,16 +42,11 @@ export default function Home() {
         >
           <SholderButtonsContainer />
         </div>
-        <div className="absolute w-1/2 top-[65%] left-1/2 -translate-x-1/2">
-          <TesterConfigs
-            showButtonsFeedFc={{ showButtonsFeed, setShowButtonsFeed }}
-          />
-        </div>
       </div>
       <div className="fixed bottom-0 left-0">
         <AboutMe />
       </div>
-      {showButtonsFeed && <div className="fixed right-0 top-28">
+      {gamepadOnline && <div className="fixed right-0 top-28">
         <BottonsFeed />
       </div>} 
     </main>
