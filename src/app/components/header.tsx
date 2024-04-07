f4478ac573191eb778309965b943faa40c27898c
Anderson David SF <andersondavidprofissional@gmail.com>
2024-04-06 22:27:28 -0300

    feat: add links to header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,9 +2,13 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between m-auto">
       <div className="p-8">
-        <h2 className="text-white text-3xl font-bold">ADFS</h2>
+        <h2 className="text-white text-3xl font-bold">GAMEPAD TESTER</h2>
+      </div>
+      <div className="">
+        <a href="#" className="text-gray-200 p-8 inline-block">GITHUB</a>
+        <a href="#" className="text-gray-200 p-8 inline-block">LINKEDIN</a>
       </div>
     </div>
   );
